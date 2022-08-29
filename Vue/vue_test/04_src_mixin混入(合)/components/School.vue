ff4dd951d380e79ed84d58376af0b7024430e21f
tom nick <nick9455279251@gmail.com>
2022-08-29 11:07:01 +0800

    上传Vue学习资料

---
+++ Vue/vue_test/04_src_mixin混入(合)/components/School.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2 @click="showName">学校名称：{{name}}</h2>
+    <h2>学校地址：{{address}}</h2>
+  </div>
+</template>
+
+<script>
+//引入一个mixin
+import { mixin, hunhe2 } from '../mixin'
+
+export default {
+  name: 'Shcool',
+  data() {
+    return {
+      name: '小学',
+      address: '广州'
+    }
+  },
+  mixins: [mixin, hunhe2]
+}
+</script>
---
+++ Vue/vue_test/05_src_插件/components/School.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2>学校名称：{{name | mySlice}}</h2>
+    <h2>学校地址：{{address}}</h2>
+    <button @click="test">点我测试一下hello方法</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'School',
+  data() {
+    return {
+      name: '白云区小学',
+      address: '广州'
+    }
+  },
+  methods: {
+    test() {
+      this.hello()
+    }
+  }
+}
+</script>
